e3bf789903d38364bd537116f53ab8c08b2e6d4e
ML <tsild@kolabnow.com>
2018-06-05 11:25:10 -0400

    Switch to more recent clojure and bump up version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject dataorigin "0.0.0-SNAPSHOT"
+(defproject dataorigin "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "GNU General Public License version 3"
             :url ""} ;; TODO
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]])
