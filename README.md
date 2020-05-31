9ffba6fc5c9842a6675ce2b472964b544cc8259a
ML <tsild@kolabnow.com>
2020-05-31 19:06:58 -0400

    Add README and set license as CC0

---
+++ README.md
@@ -0,0 +1,5 @@
+# About
+
+A small clojure library defining types to store the location of data. This library was made for my own personal use, and is not intended to be used by others.
+
+This library is mainly used with my sslib library, to preserve information about where data originated in the spreadsheet even as transformations are applied to it.
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dataorigin "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "GNU General Public License version 3"
-            :url ""} ;; TODO
+  :description ""
+  :url ""
+  :license {:name "CC0 Public Domain Dedication"
+            :url "https://creativecommons.org/publicdomain/zero/1.0/legalcode.txt"}
   :dependencies [[org.clojure/clojure "1.9.0"]])
